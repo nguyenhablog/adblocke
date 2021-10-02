29ea4fe50dc4f1c37577321ecd29ff8cef32cd88
huuha0710 <88315451+huuha0710@users.noreply.github.com>
2021-10-02 16:08:51 +0700

    Create adblock.js

---
+++ adblock.js
@@ -0,0 +1,2 @@
+// Anti Adblock
+function downloadJSAtOnload(){var e=document.createElement("script");e.src="https://cdn.jsdelivr.net/gh/nguyenhablog/adblockr@b9b6e05/adb.js",document.body.appendChild(e)}window.addEventListener?window.addEventListener("load",downloadJSAtOnload,!1):window.attachEvent?window.attachEvent("onload",downloadJSAtOnload):window.onload=downloadJSAtOnload;
